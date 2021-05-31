162770831fb06099ab069e9388ed97c35145c3ae
Damien Thiesson <damien.thiesson@gmail.com>
2021-05-31 16:35:46 +1000

    Delete Eslint comments

--- examples/remote-subscriber/events.js
+++ examples/remote-subscriber/events.js
@@ -1,5 +1,3 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-
 exports.consume = async (event, context) => {
   console.log(`Remote Eventbridge event received:`, event);
   return { statusCode: 200, body: JSON.stringify(event) };
--- examples/same-stack-publisher-subscriber/events.js
+++ examples/same-stack-publisher-subscriber/events.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 const AWS = require("aws-sdk");
 
 exports.publish = async () => {
